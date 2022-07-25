0fe41a17fad33b2ffb8bb5946a0bf3582a47f904
Letimberg Cavalcante <letimberg.cavalcante@gmail.com>
2022-07-25 20:49:23 -0300

    [javascript][dados] #4 - Aprendeno sobre variáveis e contantes

--- iniciano-com-programacao/controllers/dados._3.js
+++ iniciano-com-programacao/controllers/dados._3.js
@@ -4,6 +4,14 @@ let idade = 31
 console.log(typeof 31)
 console.log(typeof idade)
 
-
 let salario = 4578.32
 console.log(typeof salario)
+
+//Valor verdadeiro ou falso
+
+let estaChovendo = true // or false
+console.log(typeof estaChovendo) // Boolean
+
+console.log(typeof 'teste') // String
+
+// Três tipos básico suportados por js, number, boolean e string
