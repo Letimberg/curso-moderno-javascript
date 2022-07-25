3fb5689c9fb92d23746c1f8a3e986ec3fd4876e2
Letimberg Cavalcante <letimberg.cavalcante@gmail.com>
2022-07-25 20:16:55 -0300

    [javascript][dados] #3 - Apredendo sobre números

---
+++ iniciano-com-programacao/controllers/dados._3.js
@@ -0,0 +1,9 @@
+let idade = 31
+
+//Sempre que trabalhar com valores inteiros em JS, temos um number
+console.log(typeof 31)
+console.log(typeof idade)
+
+
+let salario = 4578.32
+console.log(typeof salario)
